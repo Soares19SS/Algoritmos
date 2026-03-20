20f0c88a3c05a9f82388ba1bfb00fa910a3abb30
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2026-03-20 17:22:56 -0300

    Add files via upload

---
+++ listaEx1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, n3, n4, soma;
+
+    printf("Adiciones os numeros:\n");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    scanf("%d", &n3);
+    scanf("%d", &n4);
+
+
+    soma = n1 + n2 + n3 + n4;
+
+    printf("A soma dos numeros e: \n %d", soma);
+
+    return 0;
+}
