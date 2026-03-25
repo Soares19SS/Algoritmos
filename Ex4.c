93e234235ebcaec22eac403e5c6bbca6fcf38ffe
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2026-03-25 15:37:43 -0300

    Operadores de incremento

---
+++ Ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { 
+
+    int valor = 0;
+
+    printf("%i", valor++);
+    printf("%i", ++valor);
+    printf("%i", valor--);
+    printf("%i", --valor);
+
+    return 0;
+
+}
