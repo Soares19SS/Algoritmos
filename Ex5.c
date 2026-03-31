1a4b48217522c7ba75c6ae3a1a7cbb85bd257859
Gabriel Soares <151212735+Soares19SS@users.noreply.github.com>
2026-03-31 13:08:32 -0300

    Add random number generation in Ex5.c

---
+++ Ex5.c
@@ -0,0 +1,14 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    srand(time(NULL));
+    
+    int r = rand() % 21;
+    
+    printf("%i", r);
+
+}
